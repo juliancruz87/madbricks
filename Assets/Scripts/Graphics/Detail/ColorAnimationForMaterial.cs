7d637a8dbfef993ac4633a0d82ef43f926cd02a4
Julian Cruz <juliancruz87@hotmail.com>
2015-11-18 16:36:20 -0500

    create zone highlight

--- Assets/Scripts/Graphics/Detail/ColorAnimationForMaterial.cs
+++ Assets/Scripts/Graphics/Detail/ColorAnimationForMaterial.cs
@@ -3,15 +3,26 @@ using System.Collections;
 
 namespace Graphics.Detail
 {
-	public class ColorAnimationForMaterial : MonoBehaviour {
-		public float duration = 1.0F;
-		public Color color0 = Color.red;
-		public Color color1 = Color.blue;
-		public Material material;
-		void Start() {
+	public class ColorAnimationForMaterial : MonoBehaviour 
+	{
+		[SerializeField]
+		private float duration = 1.0F;
+
+		[SerializeField]
+		private Color color0 = Color.red;
+
+		[SerializeField]
+		private Color color1 = Color.blue;
+
+		private  Material material;
+
+		private void Start() 
+		{
 			material = GetComponent<Renderer>().material;
 		}
-		void Update() {
+
+		private void Update() 
+		{
 			float t = Mathf.PingPong(Time.time, duration) / duration;
 			material.color = Color.Lerp(color0, color1, t);
 		}
