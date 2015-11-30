3c49e6c50da1b546f2629f25a9e6a366aef76915
felipefagua <felipefagua@gmail.com>
2015-11-30 11:44:33 -0500

    Felipe: Level construction ended
    The following items were added to the level 1-1 map:
    - Totems.
    - Goals.
    - Launchers.

--- Assets/Scripts/Interactive/Detail/BeginStepPathBuilder.cs
+++ Assets/Scripts/Interactive/Detail/BeginStepPathBuilder.cs
@@ -9,14 +9,22 @@ namespace Interactive.Detail {
         [SerializeField]
         private Transform mapTransform;
 
+        private Transform pathTransform;
+
         public override void StartStep() {
             CreatePath();
         }
 
         private void CreatePath() {
-            Instantiate(pathPrototype, mapTransform.position, mapTransform.rotation);
+            pathTransform = ((GameObject) Instantiate(pathPrototype, mapTransform.position, mapTransform.rotation)).transform;
             if (EndStep != null)
                 EndStep();
+        }
+
+        private void Update() {
+            if (pathTransform != null &&
+                mapTransform != null)
+            pathTransform.position = mapTransform.position;
         }
     }
 }
---
+++ Assets/Scripts/Map/MapObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Map {
+    public class MapObject : MonoBehaviour {
+
+        [SerializeField]
+        private Transform startPosition;
+
+        [SerializeField]
+        private Vector3 startPositionOffset;
+
+        // Use this for initialization
+        private void Awake() {
+            if (startPosition != null)
+                transform.position = startPosition.position + 
+                                        startPositionOffset;
+        }
+    }
+}
