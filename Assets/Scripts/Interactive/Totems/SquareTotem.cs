67bef5a571a2e56dcfde8c6e6d18cd63bb42ed6e
Julian Cruz <juliancruz87@hotmail.com>
2015-12-29 15:33:37 -0500

    fixed square totem

--- Assets/Scripts/Interactive/Totems/SquareTotem.cs
+++ Assets/Scripts/Interactive/Totems/SquareTotem.cs
@@ -31,7 +31,7 @@ namespace Interactive.Detail
 
 		private List<Node> GetNodesToTravel ()
 		{
-			List<Node> nodes = finder.FindShorterPathFromTo (CurrentNode.Id, totem.PositionToGo, Finder);//Finder.GetNodes (CurrentNode, positionToGo);
+			List<Node> nodes = Finder.GetNodes (CurrentNode, positionToGo);
 			if (nodes.Count <= 0)
 			{
 				Debug.LogWarning (gameObject.name +" try to find an alternative path");
