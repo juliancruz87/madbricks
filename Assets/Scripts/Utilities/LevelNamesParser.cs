e24ace7b76e4a5b28a67a918fcf19cdb1fe16379
Daniel Robledo <odin2014@gmail.com>
2015-12-30 02:08:24 -0500

    Updated tutorials and added the names for the levels.

---
+++ Assets/Scripts/Utilities/LevelNamesParser.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class LevelNameParser
+{
+    //public TextAsset quoteList;
+
+    public static string[] ParseNames()
+    {
+        TextAsset quoteList = Resources.Load<TextAsset>("Texts/LevelNames");
+        string[] lines = quoteList.text.Split("\n"[0]);
+        Resources.UnloadAsset(quoteList);
+        return lines;
+    }
+}
