1ec74b04d3c59b8911eec298266a7363e8ce4e85
David Vasquez <davcamvas@hotmail.com>
2016-04-18 09:43:07 -0500

    Tutorial 5

---
+++ Assets/Scripts/Interactive/Detail/tutorial steps/TapTotemTutorialStep.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using Drag;
+using Map;
+using System.Collections.Generic;
+using ManagerInput;
+
+namespace Interactive.Detail
+{
+    public class TapTotemTutorialStep : TutorialStepBase {
+
+        [SerializeField]
+		private int activeTotemPosition;
+
+        private Totem[] totems;
+
+		private Collider totemCollider;
+
+        protected override void BeginTutorialStep()
+        {
+            totems = FindObjectsOfType<Totem>();
+            ConfigTotems(activeTotemPosition);
+            ShowStartText();
+        }
+
+        private void Update()
+        {
+            if(totemCollider != null && TouchChecker.WasTappingFromCollider(Camera.main, totemCollider))
+            {
+                ShowIdleText();
+                totemCollider = null;
+            }
+        }
+
+		private void ConfigTotems (int totemPosition) 
+		{
+			foreach(Totem totem in totems)
+			{
+				if (totem.InitialPosition == totemPosition)
+                    totemCollider = totem.GetComponentInChildren<Collider>();
+			}
+		}
+    }
+}
