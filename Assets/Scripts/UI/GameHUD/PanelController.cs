dc53abcc60c948fd1e53d9be918cf2321a1fede4
Gabriel Vasco <gabriel.vasco@gmail.com>
2016-04-28 19:07:16 -0500

    Some visual Changes

---
+++ Assets/Scripts/UI/GameHUD/PanelController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PanelController : MonoBehaviour {
+
+	[SerializeField]
+
+
+
+	private void Start () {
+	
+	}
+	
+
+	private void Update () {
+	
+	}
+		
+}
